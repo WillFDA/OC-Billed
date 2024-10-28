ab60ec5f34dfdc8064c6ec1581327fb1d0bc9030
William <111514062+WillFDA@users.noreply.github.com>
2024-10-28 15:01:32 +0100

    Newbill coverage start

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,18 +1,17 @@
 /**
  * @jest-environment jsdom
  */
-
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { fireEvent, screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test("Then i can only put a image (PDF, JPG, PNG) for proof", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
       //to-do write assertion
-    })
-  })
-})
+    });
+  });
+});
